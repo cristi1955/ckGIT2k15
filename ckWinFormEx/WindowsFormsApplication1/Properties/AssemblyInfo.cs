99db0947dea8ff29b12142121d194c96a3a9cc07
cristi1955 <ck@fmi.unibuc.ro>
2014-10-06 01:47:56 +0300

    Forma Initiala

--- ckWinFormEx/WindowsFormsApplication1/Properties/AssemblyInfo.cs
+++ ckWinFormEx/WindowsFormsApplication1/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f682e5cd-09bd-4462-86ca-3ad6dba797a8")]
+[assembly: Guid("612f5718-0951-4e5c-aab9-96637d89872b")]
 
 // Version information for an assembly consists of the following four values:
 //
